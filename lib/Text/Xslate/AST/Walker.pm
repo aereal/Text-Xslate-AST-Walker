84d19382b96fdbc534dd3f83ae50a30d432b15d8
aereal <aereal@aereal.org>
2014-09-09 17:43:22 +0900

    Search descendent nodes recursively

--- lib/Text/Xslate/AST/Walker.pm
+++ lib/Text/Xslate/AST/Walker.pm
@@ -7,4 +7,46 @@ use Class::Accessor::Lite (
   ro => [qw(nodes)],
 );
 
+sub search_descendants {
+  my ($self, $predicate) = @_;
+  return [ map { @{$self->_recursive_search($_, $predicate)} } @{$self->nodes} ];
+}
+
+sub _recursive_search {
+  my ($self, $node, $predicate) = @_;
+  my @matched;
+  push @matched, $node if $predicate->($node);
+  my @matched_descendants = map { @{$self->_recursive_search($_, $predicate)} } @{$node->child_symbols};
+  push @matched, @matched_descendants;
+  return \@matched;
+}
+
+package
+  Text::Xslate::Symbol;
+
+use strict;
+use warnings;
+
+sub child_symbols {
+  my ($self) = @_;
+  return [
+    grep { $_->isa(ref($self)) }
+    @{$self->child_nodes}
+  ];
+}
+
+sub child_nodes {
+  my ($self) = @_;
+  return [
+    grep { defined($_) }
+    map { @{_ensure_array_ref($self->$_)} }
+    qw(first second third)
+  ];
+}
+
+sub _ensure_array_ref {
+  my ($maybe_arrayref) = @_;
+  return ref($maybe_arrayref) eq 'ARRAY' ? $maybe_arrayref : [$maybe_arrayref];
+}
+
 1;
